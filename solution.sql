dc3c6bbfe9e7d6774912b272839f8f1879ac6a51
vinothini149 <vinothiniav1409@gmail.com>
2026-08-21 16:04:00 +0530

    PROGRAM 4-5

--- solution.sql
+++ solution.sql
@@ -1,20 +1,20 @@
 CREATE DATABASE tusionDB;
 USE tusionDB;
-CREATE TABLE course(
-    courseID INT(5) PRIMARY KEY,
-    course name VARCHAR(200)NOT NULL,
-    credits INT,
-    departmentID VARCHAR(10)NOT NULL
+CREATE TABLE Course(
+    CourseID INT(5) PRIMARY KEY,
+    CourseName VARCHAR(20)NOT NULL,
+    Credits INT,
+    DepartmentID VARCHAR(10)NOT NULL
 );
 INSERT INTO
-    course(courseID,course name, credits, departmentID)
+    Course(CourseID,CourseName,Credits,DepartmentID)
 VALUES
-(101,'database management system ',4,1),
+(101,'database management system',4,1),
 (102,'data structures',3,2),
 (103,'computer networks',4,3);
 
-SELECT*FROM course;
-INSERT INTO student(studentID,studentName,gender, departmentID)
+SELECT*FROM Course;
+INSERT INTO Student(StudentID,StudentName,Gender,DepartmentID)
 VALUES
     (1001,'arun','male',101),
     (1002,'divya','female',102),
