782ef83c37ff7f97e3efe503a4179404e65352ad
vinothini149 <vinothiniav1409@gmail.com>
2026-08-21 16:38:27 +0530

    PROGRAM 4-5

--- solution.sql
+++ solution.sql
@@ -1,29 +1,32 @@
-CREATE DATABASE collegeDB;
-USE collegeDB;
+CREATE DATABASE tusionDB;
+USE tusionDB;
+
 CREATE TABLE Course(
-    CourseID INT(5) PRIMARY KEY,
-    CourseName VARCHAR(20)NOT NULL,
+    CourseID INT PRIMARY KEY,
+    CourseName VARCHAR(20) NOT NULL,
     Credits INT,
-    DepartmentID VARCHAR(10)NOT NULL
+    DepartmentID VARCHAR(10) NOT NULL
 );
-INSERT INTO Course(
-    CourseID,CourseName,Credits,DepartmentID)
+
+INSERT INTO Course(CourseID, CourseName, Credits, DepartmentID)
 VALUES
-(101,'database management system',4,1),
-(102,'data structures',3,2),
-(103,'computer networks',4,3);
-SELECT*FROM Course;
+(101, 'database management system', 4, 1),
+(102, 'data structures', 3, 2),
+(103, 'computer networks', 4, 3);
+
+SELECT * FROM Course;
+
 CREATE TABLE Student(
     StudentID INT PRIMARY KEY,
-    StudentName VARCHAR(20),
+    StudentName VARCHAR(20) NOT NULL,
     Gender VARCHAR(10),
-    DepartmentID VARCHAR(10)
+    DepartmentID VARCHAR(10) NOT NULL
 );
-INSERT INTO Student(
-    StudentID,StudentName,Gender,DepartmentID)
-VALUES
-    (1001,'arun','male',101),
-    (1002,'divya','female',102),
-    (1003,'karthik','male',101);
-SELECT*FROM Student;
 
+INSERT INTO Student(StudentID, StudentName, Gender, DepartmentID)
+VALUES
+(1001, 'arun', 'male', 101),
+(1002, 'divya', 'female', 102),
+(1003, 'karthik', 'male', 101);
+
+SELECT * FROM Student;
