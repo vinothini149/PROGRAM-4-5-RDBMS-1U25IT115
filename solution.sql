81a1d02c11890b7db489d7628944119f456c0240
vinothini149 <vinothiniav1409@gmail.com>
2026-08-21 16:18:48 +0530

    PROGRAM 4-5
    Reformatted INSERT statement for better readability.

--- solution.sql
+++ solution.sql
@@ -12,13 +12,12 @@ VALUES
 (101,'database management system',4,1),
 (102,'data structures',3,2),
 (103,'computer networks',4,3);
-
 SELECT*FROM Course;
-INSERT INTO Student(StudentID,StudentName,Gender,DepartmentID)
+INSERT INTO Student(
+    StudentID,StudentName,Gender,DepartmentID)
 VALUES
     (1001,'arun','male',101),
     (1002,'divya','female',102),
     (1003,'karthik','male',101);
-
 SELECT*FROM student;
 
